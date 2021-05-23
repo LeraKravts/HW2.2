5f0ffd1d98aa50fe3ff6726e2144d48f5437dfce
ValeriaKravtsova <lera.gor@inbox.ru>
2021-05-23 21:48:15 +0300

    added 3 sliders and color mix

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redSllider: UISlider!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var color: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLabel.text = String(redSllider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
+        color.backgroundColor = UIColor(red: CGFloat(redSllider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
     }
 
-
+    @IBAction func redSliderOnChange() {
+        redLabel.text = String(Float(round(100 * redSllider.value)/100))
+        color.backgroundColor = UIColor(red: CGFloat(redSllider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    @IBAction func greenSliderOnChange() {
+        greenLabel.text = String(Float(round(100 * greenSlider.value)/100))
+        color.backgroundColor = UIColor(red: CGFloat(redSllider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
+    
+    @IBAction func blueSliderOnChange() {
+        blueLabel.text = String(Float(round(100 * blueSlider.value)/100))
+        color.backgroundColor = UIColor(red: CGFloat(redSllider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
+    }
 }
-
